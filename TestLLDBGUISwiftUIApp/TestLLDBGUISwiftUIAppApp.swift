a16347288f42bcdf68f4f543e85ca1dd7ab0e843
Kim David Hauser <kim@kimhauser.ch>
2025-09-30 13:41:16 +0200

    Initial Commit

---
+++ TestLLDBGUISwiftUIApp/TestLLDBGUISwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLLDBGUISwiftUIAppApp.swift
+//  TestLLDBGUISwiftUIApp
+//
+//  Created by Kim-David Hauser on 30.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestLLDBGUISwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
